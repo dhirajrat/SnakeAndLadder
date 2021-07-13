9b2f4444d44af6adf56a3c9fdea5a0a3bbde625c
dhirajrathod <dhirajrathod3000@gmail.com>
2021-07-13 12:21:05 +0530

    Added: Added UC1

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -3,6 +3,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		
+		int currentPosition =0;
+		System.out.println("Initial Position is "+currentPosition);
 	}
 
 }
