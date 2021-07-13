de15edbcc867afe2e2b51466a8ea00987457a834
dhirajrathod <dhirajrathod3000@gmail.com>
2021-07-13 13:34:38 +0530

    Added: added to UC3

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,5 +1,10 @@
 
 public class SnakeAndLadder {
+	
+	static final int noPlay = 0;
+	static final int ladder = 1;
+	static final int snake = 2;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		
@@ -8,6 +13,31 @@ public class SnakeAndLadder {
 		
 		int die = (int) Math.floor(Math.random()*10)%6+1;
 		System.out.println("die is "+die);
+		
+		int option = (int) Math.floor(Math.random()*10)%3;
+		
+
+		
+		switch(option)
+		{
+		case noPlay:
+			System.out.println("No change- Position is = "+currentPosition);
+			
+			break;
+		case ladder:
+			currentPosition = currentPosition + die;
+			System.out.println("Goes Ahead by " + die +" Position is = "+currentPosition);
+			
+			break;
+		case snake:
+			currentPosition = currentPosition - die;
+			System.out.println("Goes Behind by "+ die + " Position is = "+currentPosition);
+			
+			break;
+		default:
+			break;
+		}
+	
 	}
 
 }
